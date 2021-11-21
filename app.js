51f79831ed6d2f1dac8a50080dd4b26107ce9f33
Kimdonghyun <dk58319@ajou.ac.kr>
2021-11-21 20:42:26 +0900

    object

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
- let sth=null;
+// array on Javascript
+//use [,]
+// object 
+const player = {
+    name : "nico" ,
+    points : 10,
+    fat: true,
+};
 
- console.log(sth);
+console.log(player.fat);
