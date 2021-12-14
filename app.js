e44ba337caba9ec6c1e71ebde44d2858c333798b
Donghyun Kim <dh58319@naver.com>
2021-12-14 23:35:42 +0900

    Lets build app 1

--- app.js
+++ app.js
@@ -1,36 +1,8 @@
-// array on Javascript
-//use [,]
-// object 
-/*const player = {
-    name : "nico" ,
-    points : 10,
-    fat: true,
-};
- 
+const loginForm = document.getElementById("login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
 
-//function 2.7 
- 
-function sayHello(){
-    //what ever I write here, it will execute 
-    
+function onLoginButtonClick(){
+    console.log(loginInput.value);
 }
-
-//cosnt age = prompt(message, default) -> pause until user make resoponce
-// how to change type 
-// parseInt() converse string into intager.
-//
-
-//git init
-
-const title = document.getElementById("title"); 
-console.dir(title);
-title.innerText = "Got you!"; 
-*/
-
-const h1 = document.querySelector("div.hello:first-child h1");
-
-function handleTitleClick(){
-    const clickedClass = "clicked";
-    h1.classList.toggle(clickedClass);
-}
-h1.addEventListener("click",handleTitleClick);
+loginButton.addEventListener("click",onLoginButtonClick );
