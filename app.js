5ece07b686c036e900d9b01724ea1589a499f9ee
Donghyun Kim <dh58319@naver.com>
2021-12-17 00:18:36 +0900

    event

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a"); 
 
-function onLoginSubmit(){
-    const username = loginInput.value;
-    console.log(username); 
+function onLoginSubmit(event){
+    event.preventDefault();
+    //const username = loginInput.value;
+    console.log(loginInput.value); 
+}
+function handleLinkEvent(event){
+    console.log(event);
+    alert("clicked");
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkEvent);
