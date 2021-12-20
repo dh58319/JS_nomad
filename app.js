cbf91dcaa9b7df828a007c3929c2d4dd43facc09
Donghyun Kim <dh58319@naver.com>
2021-12-20 15:41:28 +0900

    add Greetings

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a"); 
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
 
 function onLoginSubmit(event){
     event.preventDefault();
-    //const username = loginInput.value;
-    console.log(loginInput.value); 
+    const userName = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME); 
+    greeting.innerHTML= `Hello ${userName}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
-function handleLinkEvent(event){
-    console.log(event);
-    alert("clicked");
-}
+
 
 loginForm.addEventListener("submit", onLoginSubmit);
 link.addEventListener("click", handleLinkEvent);
+ 
